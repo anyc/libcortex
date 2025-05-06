f4a99a7616aa15a983c672b0773abfec7807e792
anyc <dev@kicherer.org>
2025-05-06 20:43:22 +0000

    Deploying to gh-pages from @ anyc/libcortex@e95baf55b26224a02b31f1189c8b6d544b30896f 🚀

--- master/search/all_4.js
+++ master/search/all_4.js
@@ -4,5 +4,5 @@ var searchData=
   ['event_20loop_1',['Why do I need an event loop?',['../index.html#autotoc_md0',1,'']]],
   ['event_5ftype_5fmatch_2',['event_type_match',['../structcrtx__task.html#ad33aa5759e298a26ca4102ce612a471e',1,'crtx_task']]],
   ['evloop_5ffd_3',['evloop_fd',['../structcrtx__listener__base.html#ae5857acce13d706148beb82de92e80bc',1,'crtx_listener_base']]],
-  ['example_4',['Example',['../index.html#autotoc_md1',1,'']]]
+  ['examples_4',['Examples',['../index.html#autotoc_md1',1,'']]]
 ];
